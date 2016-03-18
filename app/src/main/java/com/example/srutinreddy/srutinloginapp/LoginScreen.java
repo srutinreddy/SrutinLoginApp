dc58d82420d6c1b456703edfa03207795138d052
srutinreddy <srutinreddy@yahoo.com>
2016-03-17 21:44:04 -0400

    2

--- app/src/main/java/com/example/srutinreddy/srutinloginapp/LoginScreen.java
+++ app/src/main/java/com/example/srutinreddy/srutinloginapp/LoginScreen.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,7 +14,8 @@ import com.srutin.myDb;
 
 public class LoginScreen extends AppCompatActivity {
     private EditText unm, pwd;
-    //private Button login,closeApp,signUp, clear, settings;
+
+    private Button login,closeApp,signUp, clear, settings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
